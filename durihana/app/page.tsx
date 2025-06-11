30ca59853560202ab3674b895fa44ec7c99c2b13
youmin <super_vet@naver.com>
2025-06-11 14:44:28 +0900

    feat: drawerComponent

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import DrawerComponent from '@/components/atoms/DrawerComponent';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      {' '}
+      <DrawerComponent />
+    </>
+  );
 }
