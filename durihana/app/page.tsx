e806c8bbffe68f61546198133652eafb09e13ce4
chaeyoung103 <thdcodud103@naver.com>
2025-06-11 14:43:23 +0900

    fix: 테스트용 코드 제거

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,20 +1,3 @@
-import Txt from '@/components/atoms/Txt';
-
 export default function Home() {
-  return (
-    <>
-      <Txt
-        size={16}
-        color='textgray'
-        weight='regular'
-        height={20}
-        align='center'
-      >
-        Welcome to the application!
-      </Txt>
-      <Txt size={20} color='mainBlack' weight='bold' height={24}>
-        Hello World
-      </Txt>
-    </>
-  );
+  return <></>;
 }
