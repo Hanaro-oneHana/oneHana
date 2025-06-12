61b6cf14cb6f8ed72004e8579834889be0658dfe
youmin <super_vet@naver.com>
2025-06-12 13:09:33 +0900

    refactor: page 수정

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import ReservationComponent from '@/components/ReservationComponent';
-
 export default function Home() {
-  return (
-    <>
-      <ReservationComponent />
-    </>
-  );
+  return <></>;
 }
