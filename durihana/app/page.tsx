e7a233ee360ecbb1b990c100701607d9b4e9ea09
chaeyoung103 <thdcodud103@naver.com>
2025-06-12 10:34:41 +0900

    feat: 홈화면에  바텀네비게이션 추가

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,13 +1,9 @@
 import BottomNavigation from '@/components/atoms/BottomNavigation';
-import Txt from '@/components/atoms/Txt';
 
 export default function Home() {
   return (
     <>
-      <Txt size='text-[11px]' weight='font-[600]' color='text-icon'>
-        홈
-      </Txt>
-      <BottomNavigation />
+      <BottomNavigation selectedItem='home' />
     </>
   );
 }
