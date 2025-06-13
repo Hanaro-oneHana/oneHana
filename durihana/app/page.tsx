5ea37beeae24a76963dff611770ab25bc1b397ae
Kunbam7 <kunh9905@gmail.com>
2025-06-13 13:38:15 +0900

    finalized

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,18 +1,12 @@
 import BottomNavigation from '@/components/atoms/BottomNavigation';
-import Dropdown from '@/components/atoms/Dropdown';
 import Header from '@/components/atoms/Header';
 
 export default function Home() {
-  const items: string[] = [
-    'showmethemoney', 'blackshipwall', 'poweroverwelming'
-  ];
 
   return (
     <>
       <Header leftIcon='my' rightIcon='bell' />
       <BottomNavigation selectedItem='home' />
-
-      <Dropdown defaultTitle='치트키' items={items} width='w-[325px]' bgColor='bg-primarycolor'/>
     </>
   );
 }
