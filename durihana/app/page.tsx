043fa189dcfee68626ac70f7e1a0a914c43fb070
youmin <super_vet@naver.com>
2025-06-14 23:45:29 +0900

    refactor: dropdown menu 임시 설치

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,12 +1,12 @@
+import ReservationComponent from '@/components/ReservationComponent';
 import BottomNavigation from '@/components/atoms/BottomNavigation';
 import Header from '@/components/atoms/Header';
 
-
 export default function Home() {
-
   return (
     <>
       <Header leftIcon='my' rightIcon='bell' />
+      <ReservationComponent />
       <BottomNavigation selectedItem='home' />
     </>
   );
