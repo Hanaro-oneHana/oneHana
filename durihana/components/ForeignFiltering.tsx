3b0d4ed5bb769a55d389b8910ae0b29ffd07a9f5
lavendmin <lavendmin639@gmail.com>
2025-06-15 19:18:46 +0900

    feat: ForeignFiltering

---
+++ durihana/components/ForeignFiltering.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+import Button from './atoms/Button';
+import HorizontalSlider from './atoms/HorizontalSlider';
+
+export default function ForeignFiltering() {
+  const regions = [
+    '추천 여행지',
+    '국내',
+    '아시아',
+    '유럽',
+    '미주',
+    '오세아니아',
+    '기타',
+  ];
+
+  const [selectedRegions, setSelectedRegions] = useState<string[]>([]);
+  console.log(selectedRegions);
+
+  const toggleRegion = (region: string) => {
+    setSelectedRegions((prev) =>
+      prev.includes(region)
+        ? prev.filter((r) => r !== region)
+        : [...prev, region]
+    );
+  };
+  return (
+    <>
+      <HorizontalSlider>
+        <div className='flex gap-[7px]'>
+          {regions.map((region) => (
+            <Button
+              key={region}
+              onClick={() => toggleRegion(region)}
+              className={`rounded-10px px-[12px] py-[7px] text-[12px] whitespace-nowrap
+                      transition-colors duration-200
+                      ${
+                        selectedRegions.includes(region)
+                          ? 'bg-primaryhalf text-mainblack'
+                          : 'bg-buttonlightgray text-mainblack'
+                      }
+                    `}
+            >
+              {region}
+            </Button>
+          ))}
+        </div>
+      </HorizontalSlider>
+    </>
+  );
+}
