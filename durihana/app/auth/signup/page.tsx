e967e6b814db2f8166639e499b3fa5c5cd7be411
Kunbam7 <kunh9905@gmail.com>
2025-06-16 10:50:20 +0900

    signup design set

--- durihana/app/auth/signup/page.tsx
+++ durihana/app/auth/signup/page.tsx
@@ -12,8 +12,9 @@ import { ChangeEvent, useState } from "react";
 
 
 export default function signup() {
-    const position = "mt-[30px] ml-[25px]";
-    const inpuSet = "mt-[10px] ml-[25px] text-[14px]";
+    const title = "text-[16px] mt-[11px] ml-[25px] font-[500]";
+    const inpuSet = "mt-[10px] ml-[25px] w-[325px] text-[14px] font-[600] block mx-auto text-primarycolor";
+    const errMasseage = "text-[8px] text-secondaycolor mt-[3px] ml-[25px] ";
     // name, email, password, password confrim, phoneN, Mdate
     // start txt position 40,25
     // 10 txt-input group
@@ -31,14 +32,11 @@ export default function signup() {
         marriageDate: '',
     });
 
-
     const [status, setStatus] = useState<'idle' | 'success' | 'error'>('idle');
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         getUserInfo(prev => ({ ...prev, [name]: value }));
     };
-
-    // 버튼 클릭 시 FormData 만들어서 서버 액션 호출
 
     const router = useRouter();
     const handleSubmit = async () => {
@@ -56,62 +54,69 @@ export default function signup() {
         formData.append('marriageDate', userInfo.marriageDate);
 
         try {
-     const res = await fetch("/api/CreateUser", {
-       method: "POST",
-       headers: { "Content-Type": "application/json" },
-       body: JSON.stringify({
-         name: userInfo.name,
-         email: userInfo.email,
-         password: userInfo.password,
-         phone: userInfo.phone,
-         marriageDate: userInfo.marriageDate,
-       }),
-     });
-     const data = await res.json();
-     if (!res.ok || !data.success) throw new Error(data.error || "회원가입 실패");
-     setStatus("success");
-   } catch (err) {
-     console.error(err);
-     setStatus("error");
-   }
+            const res = await fetch("/api/CreateUser", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    name: userInfo.name,
+                    email: userInfo.email,
+                    password: userInfo.password,
+                    phone: userInfo.phone,
+                    marriageDate: userInfo.marriageDate,
+                }),
+            });
+            const data = await res.json();
+            if (!res.ok || !data.success) throw new Error(data.error || "회원가입 실패")
+                setStatus("success");
+            } 
+        catch (err) {
+            console.error(err);
+            setStatus("error");
+        }
     };    
-    
-    
-    
     
     // check input form
     // const formCheck = userInfo.password === userInfo.passwordCheck;
     // const [rightWritten, wrongWritten] = useReducer((check) => !check, false);  // show message for each input
     // if( userInfo.password === userInfo.passwordCheck ) wrongWritten();  
-    
+    /// disabled = {!formCheck} onClick = {}
+
+    // app/signup/page.tsx
     
     return <>
-        <Header leftIcon = "back" title="회원가입"/>
+        <Header leftIcon = "back" title="회원가입" />
 
         
-        <Txt weight = 'font-[500]' className="mt-[40px] ml-[25px]">이름</Txt>
-        <InputComponent width="w-[325px]" className="block mx-auto text-primarycolor" placeholder="이름"
+        <Txt weight = 'font-[500]' className="text-[16px] mt-[40px] ml-[25px]">이름</Txt>
+        <InputComponent width="w-[325px]" className={inpuSet} placeholder="이름을 입력해 주세요"
             name="name" value={userInfo.name} onChange={handleChange}/>
+        <Txt className={errMasseage}/>
         
+        <br/><Txt className={title}>이메일</Txt>
+        <InputComponent className={inpuSet} placeholder="abc@durihana.com"
+            name="email" value={userInfo.email} onChange={handleChange}/>
+        <Txt className={errMasseage}>*이메일형식이 올바르지 않습니다</Txt>
 
+        <br/><Txt className={title}>비밀번호</Txt>
+        <InputComponent className={inpuSet} placeholder="8자 이상 입력해 주세요"
+            name="password" value={userInfo.password} onChange={handleChange}/>
+        <Txt className={errMasseage}>*8자 이상 입력해주세요</Txt>
+
+        <br/><Txt className={title}>비밀번호 확인</Txt>
+        <InputComponent className={inpuSet} placeholder="8자 이상 입력해 주세요"
+            name="passwordCheck" value={userInfo.passwordCheck} onChange={handleChange}/>
+        <Txt className={errMasseage}>*비밀번호가 일치하지 않습니다</Txt>
+
+        <br/><Txt className={title}>전화번호</Txt>
+        <InputComponent className={inpuSet} placeholder="010-12324-1234"
+            name="phone" value={userInfo.phone} onChange={handleChange}/>
+        <Txt className={errMasseage}/>
+
+        <br/><Txt className={title}>결혼예정일</Txt>
+        <InputComponent className={inpuSet} placeholder="2026-01-01"
+            name="marriageDate" value={userInfo.marriageDate} onChange={handleChange}/>
         
-        <Txt>이메일</Txt>
-        <InputComponent name="email" value={userInfo.email} onChange={handleChange}/>
-        <Txt>wrong email address</Txt><br/>
-
-        <Txt>비밀번호</Txt>
-        <InputComponent name="password" value={userInfo.password} onChange={handleChange}/>
-
-        <Txt>비밀번호 확인</Txt>
-        <InputComponent name="passwordCheck" value={userInfo.passwordCheck} onChange={handleChange}/>
-
-        <Txt>전화번호</Txt>
-        <InputComponent name="phone" value={userInfo.phone} onChange={handleChange}/>
-
-        <Txt>결혼예정일</Txt>
-        <InputComponent name="marriageDate" value={userInfo.marriageDate} onChange={handleChange}/>
-        
-        <Button  type="submit" onClick={handleSubmit} className="w-[335px] h-[48px] block mx-auto mt-[56px] font-[500]" >완료</Button>
+        <Button  type="submit" onClick={handleSubmit} className="w-[335px] h-[48px] block mx-auto mt-[76px] font-[500]" >완료</Button>
 
         {status === 'success' && (
             <p>성공</p>
@@ -121,6 +126,3 @@ export default function signup() {
         )}
     </>;
 }
-// disabled = {!formCheck} onClick = {}
-
-// app/signup/page.tsx
