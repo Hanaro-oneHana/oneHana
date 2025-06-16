af2fe4d931252eba662743ad3eb891e8f30d8fba
youmin <super_vet@naver.com>
2025-06-16 17:19:34 +0900

    refactor: page.tsx 수정

--- durihana/app/page.tsx
+++ durihana/app/page.tsx
@@ -1,15 +1,10 @@
 import BottomNavigation from '@/components/atoms/BottomNavigation';
 import Header from '@/components/atoms/Header';
-import PartnerCalendar from '@/components/calendar/PartnerCalendar';
-import ReservationComponent from '@/components/reservation/ReservationComponent';
-import TestAccountCreator from '@/components/test/TestAccountCreator';
 
 export default function Home() {
   return (
     <>
       <Header leftIcon='my' rightIcon='bell' />
-      <TestAccountCreator userId={1} />
-      <PartnerCalendar partnerServiceId={1} />
       <BottomNavigation selectedItem='home' />
     </>
   );
