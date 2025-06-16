eef80b27959ea7d6ac3ebe4501c2e82c4b3640cd
chaeyoung103 <thdcodud103@naver.com>
2025-06-16 19:19:55 +0900

    feat: weddingbucket 페이지 생성

---
+++ durihana/app/weddingBucket/page.tsx
@@ -0,0 +1,9 @@
+import WeddingBucket from '@/components/weddingbucket/WeddingBucket';
+
+export default function WeddingBucketPage() {
+  return (
+    <div>
+      <WeddingBucket />
+    </div>
+  );
+}
---
+++ durihana/components/weddingbucket/WeddingBucket.tsx
@@ -0,0 +1,9 @@
+import Header from '../atoms/Header';
+
+export default function WeddingBucket() {
+  return (
+    <div className='flex flex-col items-center justify-start w-full'>
+      <Header title='웨딩 버켓' leftIcon='back' rightIcon='close' />
+    </div>
+  );
+}
