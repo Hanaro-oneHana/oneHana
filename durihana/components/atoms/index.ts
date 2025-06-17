8deb985f7fa8a45ffb60107f1097f9bdb738a859
chaeyoung103 <thdcodud103@naver.com>
2025-06-17 10:09:30 +0900

    refactor: import 문 clean 하게 변경

---
+++ durihana/components/atoms/index.ts
@@ -0,0 +1,8 @@
+export { default as Header } from './Header';
+export { default as Button } from './Button';
+export { default as Txt } from './Txt';
+export { default as InputComponent } from './InputComponent';
+export { default as HorizontalSlider } from './HorizontalSlider';
+export { default as Search } from './Search';
+export { default as CalendarComponent } from './CalendarComponent';
+export { default as BottomNavigation } from './BottomNavigation';
