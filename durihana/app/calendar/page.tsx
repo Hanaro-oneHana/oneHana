73a99ae0bfb0aa8c89b5ed6dafedc4086f915c64
youmin <super_vet@naver.com>
2025-06-18 16:34:33 +0900

    refactor: CalendarPage Header 및 session 연결

--- durihana/app/calendar/page.tsx
+++ durihana/app/calendar/page.tsx
@@ -1,10 +1,25 @@
-import BottomNavigation from '@/components/atoms/BottomNavigation';
+'use client';
+
+import { BottomNavigation, Header } from '@/components/atoms';
 import UserCalendar from '@/components/schedule/UserCalendar';
+import { useSession } from 'next-auth/react';
 
 export default function Calendar() {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <p>로딩 중...</p>;
+  }
+  if (!session?.user?.id) {
+    return <p>로그인이 필요합니다.</p>;
+  }
+  const userId = Number(session.user.id);
+
   return (
     <>
-      <UserCalendar userId={1} />
+      <Header leftIcon='my' rightIcon='bell' />
+
+      <UserCalendar userId={userId} />
       <BottomNavigation selectedItem='calendar' />
     </>
   );
