eab89b57651e8a826bebc0bb512706b0e4580767
Kunbam7 <kunh9905@gmail.com>
2025-06-18 10:18:54 +0900

    uninputErr messeage 1

--- durihana/app/auth/signup/page.tsx
+++ durihana/app/auth/signup/page.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-import Button from "@/components/atoms/Button";
-import Header from "@/components/atoms/Header";
-import InputComponent from "@/components/atoms/InputComponent";
-import Txt from "@/components/atoms/Txt";
+import { Button, Header, InputComponent, Txt } from "@/components/atoms";
 import { createAction } from "@/lib/actions/CreateActions";
 import { credentialValidator } from "@/lib/validator";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import type React from 'react';
 
 
@@ -25,66 +21,66 @@ export default function SignUp() {
         marriageDate: '',
     });
     const emailValidator = credentialValidator.safeParse({email: userInfo.email});
-    const [status, setStatus] = useState<"idle" | "success" | "error">("idle");
+    const [status, setStatus] = useState<"idle" | "success" | "error" | "inputError">("idle");
     
-    const router = useRouter()
+    const handdleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        if(Object.values(userInfo).some((v) => v.trim() === '')) {
+            e.preventDefault();
+            setStatus('inputError');
+        }
+    };
+
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         getUserInfo((prev) => ({ ...prev, [name]: value }));
     };
 
     return <>
-        <Header leftIcon = "back" title="회원가입" />
-        <form action={createAction}>
+        <Header leftIcon='back' title="회원가입" />
+        <form action={createAction} onSubmit={handdleSubmit}>
             <div>
                 <Txt className="font-[500] text-[16px] mt-[40px] ml-[25px]">이름</Txt>
                 <InputComponent width="w-[325px]" className={inputSet} placeholder="이름을 입력해 주세요"
                     name="name" value={userInfo.name} onChange={handleChange}/>
-                <Txt className={errMasseage}/>
+                <Txt className={errMasseage}>{status === 'inputError' && userInfo.name.trim() === '' ? '*이름을 입력해 주세요.' : '\u00A0'}</Txt>
             </div>
 
             <div>
                 <Txt className={title}>이메일</Txt>
                 <InputComponent className={inputSet} placeholder="abc@durihana.com"
                     type="email" name="email" value={userInfo.email} onChange={handleChange}/>
-                <Txt className="text-red text-[8px] mt-[3px] ml-[25px]" >{!emailValidator.success && userInfo.email.length > 0 ? ' *이메일형식이 올바르지 않습니다 ' : ' '}</Txt>
+                <Txt className={errMasseage} >{status === 'inputError' ? '*이메일을 입력해 주세요' : !emailValidator.success && userInfo.email.length > 0 ? ' *이메일형식이 올바르지 않습니다 ' : '\u00A0'}</Txt>
             </div>
             
             <div>
                 <Txt className={title}>비밀번호</Txt>
                 <InputComponent className={inputSet} placeholder="8자 이상 입력해 주세요"
                     type="password" name="password" value={userInfo.password} onChange={handleChange}/>
-                <Txt className={errMasseage}>{userInfo.password.length > 0 && userInfo.password.length < 8 ? ' *8자 이상 입력해주세요 ' : '  '}</Txt>
+                <Txt className={errMasseage}>{status === 'inputError' ? '*비밀번호를 입력해 주세요' :userInfo.password.length > 0 && userInfo.password.length < 8 ? ' *8자 이상 입력해주세요 ' : '\u00A0'}</Txt>
             </div>
             
             <div>
                 <Txt className={title}>비밀번호 확인</Txt>
                 <InputComponent className={inputSet} placeholder="8자 이상 입력해 주세요"
                     type="password" name="passwordCheck" value={userInfo.passwordCheck} onChange={handleChange}/>
-                <Txt className={errMasseage}>{userInfo.password !== userInfo.passwordCheck && userInfo.passwordCheck.length > 0  ? ' *비밀번호가 일치하지 않습니다 ' : ''}</Txt>
+                <Txt className={errMasseage}>{status === 'inputError' ? '*비밀번호를 다시 입력해 주세요' :userInfo.password !== userInfo.passwordCheck && userInfo.passwordCheck.length > 0  ? ' *비밀번호가 일치하지 않습니다 ' : '\u00A0'}</Txt>
             </div>
 
             <div>
                 <Txt className={title}>전화번호</Txt>
                 <InputComponent className={inputSet} placeholder="010-12324-1234"
                     name="phone" value={userInfo.phone} onChange={handleChange}/>
-                <Txt className={errMasseage}/>
+                <Txt className={errMasseage}>{status === 'inputError' ? '*전화번호를 입력해 주세요' : '\u00A0'}</Txt>
             </div>
 
             <div>
                 <Txt className={title}>결혼예정일</Txt>
                 <InputComponent className={inputSet} placeholder="2026-01-01"
                     type="date" name="marriageDate" value={userInfo.marriageDate} onChange={handleChange}/>
+                <Txt className={errMasseage}>{status === 'inputError' ? '*결혼예정일을 입력해 주세요' : '\u00A0'}</Txt>
             </div>
             
             <Button type="submit" className="w-[335px] h-[48px] block mx-auto mt-[76px] font-[500]" >완료</Button>
-            
-            {status === 'success' && (
-                <p>성공</p>
-            )}
-            {status === 'error' && (
-                <p>오류발생</p>
-            )}
         </form>
     </>;
 }
