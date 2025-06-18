7017c6c42b92382e1b2eefb2b3e9a4ed9a06a2f2
Kunbam7 <kunh9905@gmail.com>
2025-06-18 10:59:41 +0900

    unInputErr message

--- durihana/app/auth/signup/page.tsx
+++ durihana/app/auth/signup/page.tsx
@@ -49,35 +49,35 @@ export default function SignUp() {
                 <Txt className={title}>이메일</Txt>
                 <InputComponent className={inputSet} placeholder="abc@durihana.com"
                     type="email" name="email" value={userInfo.email} onChange={handleChange}/>
-                <Txt className={errMasseage} >{status === 'inputError' ? '*이메일을 입력해 주세요' : !emailValidator.success && userInfo.email.length > 0 ? ' *이메일형식이 올바르지 않습니다 ' : '\u00A0'}</Txt>
+                <Txt className={errMasseage} >{status === 'inputError' && userInfo.email.trim() === '' ? '*이메일을 입력해 주세요' : !emailValidator.success && userInfo.email.length > 0 ? ' *이메일형식이 올바르지 않습니다 ' : '\u00A0'}</Txt>
             </div>
             
             <div>
                 <Txt className={title}>비밀번호</Txt>
                 <InputComponent className={inputSet} placeholder="8자 이상 입력해 주세요"
                     type="password" name="password" value={userInfo.password} onChange={handleChange}/>
-                <Txt className={errMasseage}>{status === 'inputError' ? '*비밀번호를 입력해 주세요' :userInfo.password.length > 0 && userInfo.password.length < 8 ? ' *8자 이상 입력해주세요 ' : '\u00A0'}</Txt>
+                <Txt className={errMasseage}>{status === 'inputError' && userInfo.password.trim() === '' ? '*비밀번호를 입력해 주세요' :userInfo.password.length > 0 && userInfo.password.length < 8 ? ' *8자 이상 입력해주세요 ' : '\u00A0'}</Txt>
             </div>
             
             <div>
                 <Txt className={title}>비밀번호 확인</Txt>
                 <InputComponent className={inputSet} placeholder="8자 이상 입력해 주세요"
                     type="password" name="passwordCheck" value={userInfo.passwordCheck} onChange={handleChange}/>
-                <Txt className={errMasseage}>{status === 'inputError' ? '*비밀번호를 다시 입력해 주세요' :userInfo.password !== userInfo.passwordCheck && userInfo.passwordCheck.length > 0  ? ' *비밀번호가 일치하지 않습니다 ' : '\u00A0'}</Txt>
+                <Txt className={errMasseage}>{status === 'inputError' && userInfo.passwordCheck.trim() === '' ? '*비밀번호를 다시 입력해 주세요' :userInfo.password !== userInfo.passwordCheck && userInfo.passwordCheck.length > 0  ? ' *비밀번호가 일치하지 않습니다 ' : '\u00A0'}</Txt>
             </div>
 
             <div>
                 <Txt className={title}>전화번호</Txt>
                 <InputComponent className={inputSet} placeholder="010-12324-1234"
                     name="phone" value={userInfo.phone} onChange={handleChange}/>
-                <Txt className={errMasseage}>{status === 'inputError' ? '*전화번호를 입력해 주세요' : '\u00A0'}</Txt>
+                <Txt className={errMasseage}>{status === 'inputError' && userInfo.phone.trim() === '' ? '*전화번호를 입력해 주세요' : '\u00A0'}</Txt>
             </div>
 
             <div>
                 <Txt className={title}>결혼예정일</Txt>
                 <InputComponent className={inputSet} placeholder="2026-01-01"
                     type="date" name="marriageDate" value={userInfo.marriageDate} onChange={handleChange}/>
-                <Txt className={errMasseage}>{status === 'inputError' ? '*결혼예정일을 입력해 주세요' : '\u00A0'}</Txt>
+                <Txt className={errMasseage}>{status === 'inputError' && userInfo.marriageDate.trim() === '' ? '*결혼예정일을 입력해 주세요' : '\u00A0'}</Txt>
             </div>
             
             <Button type="submit" className="w-[335px] h-[48px] block mx-auto mt-[76px] font-[500]" >완료</Button>
