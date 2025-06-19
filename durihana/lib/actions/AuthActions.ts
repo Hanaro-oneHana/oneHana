094d027bf439f3e6ecdc8925228f76b5a25b712f
chaeyoung103 <thdcodud103@naver.com>
2025-06-19 16:34:14 +0900

    refactor: 로그인 로직 validator, bcrypt 사용하여 수정

--- durihana/lib/actions/AuthActions.ts
+++ durihana/lib/actions/AuthActions.ts
@@ -1,13 +1,53 @@
+'use server';
+
+import bcrypt from 'bcryptjs';
 import prisma from '@/lib/db';
+import { signInValidator } from '../validator';
+
+export async function signInValidateAction(email: string, password: string) {
+  // email과 password가 비어있는 경우
+  if (!email || !password) {
+    return { isSuccess: false, error: '이메일, 비밀번호를 입력해주세요' };
+  }
+
+  // 이메일,비밀번호 형식이 올바른지 확인
+  const validation = signInValidator.safeParse({ email, password });
+  if (!validation.success) {
+    return {
+      isSuccess: false,
+      error: '이메일 또는 비밀번호 형식이 올바르지 않습니다',
+    };
+  }
+
+  const user = await findUserByEmail(email);
+
+  // 존재하는 이메일인지 확인
+  if (!user.isSuccess || !user.data) {
+    return { isSuccess: false, error: '아이디가 존재하지 않습니다' };
+  }
+
+  // 비밀번호가 일치하는지 확인
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    user.data.password || ''
+  );
+  if (!isPasswordValid) {
+    return { isSuccess: false, error: '비밀번호가 올바르지 않습니다' };
+  }
+
+  // 로그인 성공
+  return { isSuccess: true, data: user.data };
+}
 
 export async function findUserByEmail(email: string) {
   try {
     const user = await prisma.user.findUnique({
       where: { email },
     });
-    return user;
+
+    return { isSuccess: true, data: user };
   } catch (error) {
-    console.error('findUserByEmail error:', error);
-    return null;
+    console.log(error);
+    return { isSuccess: false, error: 'User not found' };
   }
 }
