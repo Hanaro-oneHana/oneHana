baf15a4f7e236be9fb2f7bfd17af055a6c54a62e
lavendmin <lavendmin639@gmail.com>
2025-06-22 02:45:11 +0900

    fix: 지출 내역 없을 때 자산 페이지 화면 수정

--- durihana/components/asset/AssetChart.tsx
+++ durihana/components/asset/AssetChart.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { PieChart, Pie, Cell } from 'recharts';
+import Image from 'next/image';
 import Txt from '../atoms/Txt';
 
 type Props = {
@@ -53,42 +54,74 @@ const renderCustomizedLabel = ({
 };
 
 export default function AssetChart({ data }: Props) {
-  return (
-    <div className='flex items-center w-full'>
-      {/* 파이 차트 */}
-      <div className='flex items-center justify-center w-[170px] h-[170px] shrink-0'>
-        <PieChart width={170} height={170}>
-          <Pie
-            data={data}
-            cx={80}
-            cy={80}
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            dataKey='value'
-          >
-            {data.map((_, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-          </Pie>
-        </PieChart>
-      </div>
+  const isEmpty = data.length === 0;
 
-      {/* 항목 설명 영역 */}
-      <div className='flex flex-col justify-center ml-auto'>
-        {data.map((entry, index) => (
-          <div key={index} className='flex items-center mb-[11px] last:mb-0 '>
-            <div
-              className='w-[11px] h-[10px] mr-[22px]'
-              style={{ backgroundColor: COLORS[index % COLORS.length] }}
-            />
-            <Txt>{entry.name}</Txt>
+  return (
+    <>
+      {isEmpty ? (
+        <div className='flex flex-col items-center w-full'>
+          <Txt
+            size='text-[14px]'
+            color='text-textgray'
+            className='text-center mb-[20px]'
+          >
+            예약 및 결제 완료된 항목을 기준으로
+            <br /> 지출 데이터가 시각적으로 분석되어 표시됩니다.
+          </Txt>
+
+          <Image
+            src='/asset/images/piechart.svg'
+            alt='파이차트'
+            width={302}
+            height={170}
+            className='blur-[4px]'
+          />
+        </div>
+      ) : (
+        <>
+          <div className='flex items-center w-full'>
+            {/* 파이 차트 */}
+            <div className='flex items-center justify-center w-[170px] h-[170px] shrink-0'>
+              <PieChart width={170} height={170}>
+                <Pie
+                  data={data}
+                  cx={80}
+                  cy={80}
+                  labelLine={false}
+                  label={renderCustomizedLabel}
+                  outerRadius={80}
+                  dataKey='value'
+                >
+                  {data.map((_, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+              </PieChart>
+            </div>
+
+            {/* 항목 설명 영역 */}
+            <div className='flex flex-col justify-center ml-auto'>
+              {data.map((entry, index) => (
+                <div
+                  key={index}
+                  className='flex items-center mb-[11px] last:mb-0 '
+                >
+                  <div
+                    className='w-[11px] h-[10px] mr-[22px]'
+                    style={{
+                      backgroundColor: COLORS[index % COLORS.length],
+                    }}
+                  />
+                  <Txt>{entry.name}</Txt>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
-    </div>
+        </>
+      )}
+    </>
   );
 }
