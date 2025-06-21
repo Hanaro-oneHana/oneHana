4bae36a0dc0007709eebe414d35ca8059f449b73
chaeyoung103 <thdcodud103@naver.com>
2025-06-21 22:56:48 +0900

    fix: 데이터 받아오는 로직 수정 및 box 컴포넌트 분리

--- durihana/components/weddingbucket/WeddingBucket.tsx
+++ durihana/components/weddingbucket/WeddingBucket.tsx
@@ -1,5 +1,10 @@
+'use client';
+
 import { Header, Button, Txt } from '@/components/atoms';
 import WeddingBucketBox from '@/components/weddingbucket/WeddingBucketBox';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import AlertModal from '../alert/AlertModal';
 
 export type Option = {
   optionTitle: string;
@@ -20,40 +25,40 @@ type Props = {
 };
 
 export default function WeddingBucket({ items }: Props) {
+  const router = useRouter();
   const categories = ['예식장', '스드메', '신혼여행', '가전·가구', '예물·예단'];
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className='flex flex-col items-center justify-start w-full pt-[60px]'>
-      <Header title='웨딩 버켓' leftIcon='back' rightIcon='close' />
+      <Header
+        title='웨딩 버켓'
+        leftIcon='back'
+        rightIcon='close'
+        onRightClick={() => router.back()}
+      />
       <div className='flex flex-col items-start justify-start w-full px-[25px] pt-[20px] pb-[40px] gap-[30px]'>
         {categories.map((category, index) => (
           <div key={index} className='flex flex-col gap-[10px] w-full'>
             <Txt className='text-[16px] font-[500] '>{category}</Txt>
-            <div className='flex flex-col gap-[10px] rounded-[10px] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.05)]'>
-              <div className='relative flex items-center justify-between p-[20px] bg-mainwhite rounded-[10px]'>
-                {(() => {
-                  const filteredItems = items?.filter(
-                    (item) => item.category === index + 1
-                  );
-                  return filteredItems && filteredItems.length > 0 ? (
-                    filteredItems.map((item, idx) => (
-                      <WeddingBucketBox
-                        key={item.id}
-                        items={filteredItems}
-                        index={idx}
-                      />
-                    ))
-                  ) : (
-                    <Txt className='text-[14px] text-textgray font-[500]'>
-                      내용이 없습니다
-                    </Txt>
-                  );
-                })()}
-              </div>
-            </div>
+
+            {(() => {
+              const filteredItems = items?.filter(
+                (item) => item.category === index + 1
+              );
+              return filteredItems && filteredItems.length > 0 ? (
+                filteredItems.map((item, idx) => (
+                  <WeddingBucketBox key={item.id} item={item} index={idx} />
+                ))
+              ) : (
+                <Txt className='text-[14px] text-textgray font-[500]'>
+                  내용이 없습니다
+                </Txt>
+              );
+            })()}
           </div>
         ))}
-        <div className='flex items-center justify-between w-full '>
+        <div className='flex items-center justify-between w-full'>
           <Txt
             align='text-center'
             color='text-primarycolor'
@@ -74,7 +79,7 @@ export default function WeddingBucket({ items }: Props) {
             원
           </Txt>
         </div>
-        <Button>
+        <Button onClick={() => setIsModalOpen(true)}>
           <Txt
             size='text-[16px]'
             weight='font-[500]'
@@ -85,6 +90,37 @@ export default function WeddingBucket({ items }: Props) {
           </Txt>
         </Button>
       </div>
+      {isModalOpen && (
+        <AlertModal onClose={() => setIsModalOpen(false)}>
+          <Txt align='text-center' weight='font-[600]'>
+            선택하신 웨딩 견적에 맞춰 <br /> 신혼부부를 위한 <br /> 하나은행의
+            맞춤 <span className='text-primarycolor'>예적금·대출 서비스</span>로{' '}
+            <br /> 결혼 부담을 덜어보세요!
+          </Txt>
+          <div className='flex flex-row items-center justify-center gap-[15px] w-full mt-[25px]'>
+            <Button
+              bgColor='bg-icon'
+              onClick={() => {
+                setIsModalOpen(false);
+                router.push('/');
+              }}
+              className='py-[10px]'
+            >
+              홈으로
+            </Button>
+            <Button
+              bgColor='bg-primarycolor'
+              onClick={() => {
+                setIsModalOpen(false);
+                router.push('/account/else-account');
+              }}
+              className='py-[10px]'
+            >
+              확인
+            </Button>
+          </div>
+        </AlertModal>
+      )}
     </div>
   );
 }
--- durihana/components/weddingbucket/WeddingBucketBox.tsx
+++ durihana/components/weddingbucket/WeddingBucketBox.tsx
@@ -1,52 +1,66 @@
+'use client';
+
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 import { Button, Txt } from '../atoms';
 import { BucketItem } from './WeddingBucket';
 
 type Props = {
-  items: BucketItem[];
+  item: BucketItem;
   index: number;
 };
 
-export default function WeddingBucketBox({ items, index }: Props) {
+export default function WeddingBucketBox({ item, index }: Props) {
   const bucketState = ['예약', '예약완료', '결제', '결제완료'];
+
   return (
-    <>
-      <Button className='absolute bg-transparent p-0 top-[15px] right-[15px] w-fit h-fit leading-none'>
-        <Image
-          src={`/asset/icons/close.svg`}
-          alt={`Close Icon`}
-          width={16}
-          height={16}
-        />
-      </Button>
-      <div className='flex flex-col gap-[20px] w-full'>
-        <Txt className='text-[16px] font-[500]'>{items[index].store}</Txt>
-        <div className='flex flex-col items-center w-full gap-[4px]'>
-          {items[index].options?.map((option, idx) => (
-            <div
-              key={idx}
-              className='flex items-center justify-between text-[14px] text-textgray font-[500] w-full'
+    <div className='flex flex-col gap-[10px] rounded-[10px] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.05)]'>
+      <div className='relative flex items-center justify-between p-[20px] bg-mainwhite rounded-[10px]'>
+        <Button className='absolute bg-transparent p-0 top-[15px] right-[15px] w-fit h-fit leading-none'>
+          <Image
+            src={`/asset/icons/close.svg`}
+            alt={`Close Icon`}
+            width={16}
+            height={16}
+          />
+        </Button>
+        <div className='flex flex-col gap-[20px] w-full'>
+          <Txt className='text-[16px] font-[500]'>{item.store}</Txt>
+          <div className='flex flex-col items-center w-full gap-[4px]'>
+            {item.options?.map((option, idx) => (
+              <div
+                key={idx}
+                className='flex items-center justify-between text-[14px] text-textgray font-[500] w-full'
+              >
+                <Txt size='text-[12px]' weight='font-[500]'>
+                  {option.optionTitle}
+                </Txt>
+                <Txt
+                  size='text-[12px]'
+                  weight='font-[500]'
+                  color='text-textgray'
+                >
+                  {option.optionContent}
+                </Txt>
+              </div>
+            ))}
+          </div>
+          <div className='flex items-center justify-end w-full gap-[20px]'>
+            <Txt
+              size='text-[16px]'
+              weight='font-[500]'
+              color='text-primarycolor'
             >
-              <Txt size='text-[12px]' weight='font-[500]'>
-                {option.optionTitle}
-              </Txt>
-              <Txt size='text-[12px]' weight='font-[500]' color='text-textgray'>
-                {option.optionContent}
-              </Txt>
-            </div>
-          ))}
-        </div>
-        <div className='flex items-center justify-end w-full gap-[20px]'>
-          <Txt size='text-[16px]' weight='font-[500]' color='text-primarycolor'>
-            {items[index].price?.toLocaleString()} 원
-          </Txt>
-          <Button className='w-fit h-fit px-[10px] py-[9px] bg-mint leading-[10px] '>
-            <Txt size='text-[12px]' weight='font-[500]' align='text-center'>
-              {bucketState[items[index].state || 0]}
+              {item.price?.toLocaleString()} 원
             </Txt>
-          </Button>
+            <Button className='w-fit h-fit px-[10px] py-[9px] bg-mint leading-[10px] '>
+              <Txt size='text-[12px]' weight='font-[500]' align='text-center'>
+                {bucketState[item.state || 0]}
+              </Txt>
+            </Button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
