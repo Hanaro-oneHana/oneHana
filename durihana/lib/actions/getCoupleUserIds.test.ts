e09dcf88f70193750a485350db787dde26f93369
Kunbam7 <kunh9905@gmail.com>
2025-06-26 15:32:49 +0900

    save

--- durihana/lib/actions/getCoupleUserIds.test.ts
+++ durihana/lib/actions/getCoupleUserIds.test.ts
@@ -1,13 +1,30 @@
 
 import { getCoupleUserIds, getCoupleNames } from './getCoupleUserIds';
-import { PrismaClient } from '@/lib/generated/prisma';
 import { describe, it, expect, vi, beforeEach, Mock } from 'vitest';
+import prisma from '../db';
+
+vi.mock('../db', () => ({
+  default: {
+    user: {
+      findUnique: vi.fn(),
+      findMany: vi.fn(),
+    },
+  },
+}));
+
+const mockUser = prisma.user.findUnique as Mock;
+const mockMany = prisma.user.findMany as Mock;
 
 describe('getCoupleUserIds', () => {
-
+  beforeEach(() => {
+    vi.clearAllMocks();
+  })
 
 });
 
 describe('getCoupleNames', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  })
 
 });
